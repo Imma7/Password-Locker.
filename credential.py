af811189f6c67e1abf9cb5be4aa37375b524f025
imma <immamugambi@gmail.com>
2018-06-11 01:06:55 +0300

    Created delete account function

--- credential.py
+++ credential.py
@@ -21,3 +21,14 @@ class Credential:
         print("Account Name || UserName || Password")
         for i in cls.credential_list:
             print(f" {i['account']} || {i['username']} || {i['password']}")
+
+    # @classmethod
+    # def search_account():
+    #     pass
+
+    @classmethod
+    def delete_account(cls, acc):
+        for a in cls.credential_list:
+            if a['account'] == acc:
+                cls.credential_list.remove(a)
+    
