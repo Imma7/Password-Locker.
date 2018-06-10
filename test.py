30c563c363e9e52b1c55c0307a555849d1d9a94d
imma <immamugambi@gmail.com>
2018-06-11 02:15:55 +0300

    Created subclass class TestUser and TestCrentials

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 import unittest #Importing the unittest module
-from user import User
-from credential import Credential
+from user import User # Importing User class
+from credential import Credential #Importing Credential class
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours
+    Args: 
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+class TestCredential(unittest.TestCase):
+    '''
+    Test class that defines test cases for the credential class behavious
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
