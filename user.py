1f3537f8472ee03c28645ff0e235e30cbc1d53a4
imma <immamugambi@gmail.com>
2018-06-11 01:12:05 +0300

    User init method

--- user.py
+++ user.py
@@ -2,4 +2,8 @@ class User:
     
     user = []
 
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        User.user = {"name": self.name, "password": self.password}
     
