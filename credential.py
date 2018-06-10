2073faec5d2817c3778fa836dbe204b4716d11d9
imma <immamugambi@gmail.com>
2018-06-11 00:54:38 +0300

    Credential init method

--- credential.py
+++ credential.py
@@ -1,3 +1,9 @@
 class Credential:
 
-    
+    credential_list = []
+
+    def __init__(self, account, username, password):
+        self.account = account
+        self.username = username
+        self.password = password
+        Credential.credential_list.append({'account': self.account, 'username': self.username, 'password': self.password})
