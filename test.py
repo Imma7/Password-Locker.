3304c7ab44789b08ce345f4f9be1c6bc256b37dc
imma <immamugambi@gmail.com>
2018-06-11 10:05:37 +0300

    Set up method for User class

--- test.py
+++ test.py
@@ -9,6 +9,23 @@ class TestUser(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
 
+    def setUp(self):
+        '''
+        Set up method to run before each test cases
+        '''
+        self.new_user = User("Imma", "1234") #create user object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is instantiated properly
+        '''
+
+        self.assertEqual(self.new_user.name,"Imma")
+        self.assertEqual(self.new_user.password,"1234")
+
+
+
+
 class TestCredential(unittest.TestCase):
     '''
     Test class that defines test cases for the credential class behavious
